d4ba49a8becdb6235abcd71c016e1ef32f56defc
shernandezer.ieu2019@student.ie.edu <shernandezer.ieu2019@student.ie.edu>
2023-08-02 13:12:14 +0000

    product_margin

--- macros/margin_functions.sql
+++ macros/margin_functions.sql
@@ -1,3 +1,7 @@
 {% macro margin_percent(turnover, purchase_cost, precision=2) %}
    ROUND( SAFE_DIVIDE( ({{ turnover }} - {{ purchase_cost }}) , {{ turnover }} ) , {{ precision }})
 {% endmacro %}
+
+{% macro product_margin(turnover, purchase_cost, precision=2) %}
+   ROUND(({{ turnover }} - {{ purchase_cost }})  , {{ precision }})
+{% endmacro %}
